73aeb0b27d499ce219339434610eab102f0602e3
Junior <141065960+Mosifane@users.noreply.github.com>
2024-05-26 13:02:16 +0200

    Get input from the user.

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+const { read } = require('node:fs');
+const readline = require('node:readline');
+
 // main menu and main functionality will be written here!
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("What song do you want to search for? ", songName => {
+    console.log(`We are looking for ${songName}`)
+    // API search and output here!!
+    rl.close()
+})
